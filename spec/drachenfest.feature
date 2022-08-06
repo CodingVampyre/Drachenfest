acdf528bfe2c8f02a6dbb64906ec9e6c5cfc9fe3
Tobias Kavsek <tobiaskavsek@hotmail.de>
2022-08-06 11:21:32 +0200

    initial commit

---
+++ spec/drachenfest.feature
@@ -0,0 +1,1 @@
+Feature: selling tickets for Drachenfest
---
+++ src/main/java/com/github/codingvampyre/drachenfest/DrachenfestApplication.java
@@ -0,0 +1,13 @@
+package com.github.codingvampyre.drachenfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrachenfestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrachenfestApplication.class, args);
+    }
+
+}
